f8a9938e4af7abfca85af33f74380198d474f07b
hankakukatakana <masaki.0409.a@gmail.com>
2023-10-10 14:30:44 +0900

    app

--- gui_app/dnschange.py
+++ gui_app/dnschange.py
@@ -1,4 +1,17 @@
 import win32com.client
+import sys
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    print("管理者権限で実行してください.")
+    sys.exit()
+
 
 def set_dns_servers(interface_name, dns_servers):
     objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
